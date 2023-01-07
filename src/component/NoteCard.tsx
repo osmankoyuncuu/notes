8b78a72d2b31887d5f7f79cf8e16896c7bbd77ab
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-08 00:08:55 +0300

    fixed

--- src/component/NoteCard.tsx
+++ src/component/NoteCard.tsx
@@ -1,4 +1,7 @@
+import { Badge, Card, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { Tag } from "../App";
+import styles from "../styles/NoteList.module.css";
 
 type SimplifiedNote = {
   tags: Tag[];
@@ -7,7 +10,35 @@ type SimplifiedNote = {
 };
 
 const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
-  return <div>NoteCard</div>;
+  return (
+    <Card
+      as={Link}
+      to={`'${id}`}
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+    >
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction={"horizontal"}
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map((tag) => (
+                <Badge key={tag.id} className="text-truncate">
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default NoteCard;
